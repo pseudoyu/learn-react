28cb5f9c2823a04dd803360b34f0d386c1b8ee29
pseudoyu <pseudoyu@connect.hku.hk>
2022-10-24 18:21:03 +0800

    feat: react-with-ts-demo done

--- react-with-ts-demo/src/App.tsx
+++ react-with-ts-demo/src/App.tsx
@@ -1,31 +1,51 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
 
-  const [number, setNumber] = useState<number | string>(5);
+  // const [number, setNumber] = useState<number | string>(5);
 
-  const changeNumber = () => {
-    setNumber(10)
-  }
+  // const changeNumber = () => {
+  //   setNumber(10)
+  // }
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      age: 35,
+      url: "https://pseudoyu.oss-cn-hangzhou.aliyuncs.com/images/weekly_review_1009_photo.png",
+    },
+    {
+      name: "Kevin Durant",
+      age: 30,
+      url: "https://pseudoyu.oss-cn-hangzhou.aliyuncs.com/images/weekly_review_1009_photo.png",
+      note: "Best player in the world"
+    },
+    {
+      name: "Stephen Curry",
+      age: 31,
+      url: "https://pseudoyu.oss-cn-hangzhou.aliyuncs.com/images/weekly_review_1009_photo.png",
+      note: "MVP"
+    }
+  ]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my party</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
