b94247e39b2998b49fe46b5862f282373591efe1
pseudoyu <pseudoyu@connect.hku.hk>
2022-12-12 02:20:28 +0800

    feat: add json-server backend mock support & feedback app done

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -2,16 +2,17 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner';
 
 const FeedbackList = () => {
-    const { feedback } = useContext(FeedbackContext)
-    if (!feedback || feedback.length === 0) {
+    const { feedback, isLoading } = useContext(FeedbackContext)
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return (
             <p>No feedback</p>
         )
     }
-    return (
-        <div className="feedback-list">
+
+    return isLoading ? (<Spinner />) : (        <div className="feedback-list">
             <AnimatePresence>
                 {feedback.map((item) => (
                     <motion.div
@@ -24,8 +25,7 @@ const FeedbackList = () => {
                     </motion.div>
                 ))}
             </AnimatePresence>
-        </div>
-    )
+        </div>)
 }
 
 export default FeedbackList
--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,23 +1,45 @@
-import { createContext, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import FeedbackData from '../data/FeedbackData'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState(FeedbackData)
+	const [isLoading, setIsLoading] = useState(true)
+	const [feedback, setFeedback] = useState([])
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
 	})
 
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4()
-		setFeedback([newFeedback, ...feedback])
+	useEffect(() => {
+		fetchFeedback()
+	}, [])
+
+	const fetchFeedback = async () => {
+		const response = await fetch(`/feedback?_sort=id&_order=desc`)
+		const data = await response.json()
+		setFeedback(data)
+		setIsLoading(false)
 	}
 
-	const deleteFeedback = (id) => {
+	const addFeedback = async (newFeedback) => {
+		const response = await fetch(`/feedback`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newFeedback),
+		})
+
+		const data = await response.json()
+		setFeedback([data, ...feedback])
+	}
+
+	const deleteFeedback = async (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
+			await fetch(`/feedback/${id}`, {
+				method: 'DELETE',
+			})
+
 			setFeedback(
 				feedback.filter((item) => {
 					return item.id !== id
@@ -26,11 +48,21 @@ export const FeedbackProvider = ({ children }) => {
 		}
 	}
 
-	const updateFeedback = (id, updItem) => {
+	const updateFeedback = async (id, updItem) => {
+		const response = await fetch(`/feedback/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updItem),
+		})
+
+		const data = await response.json()
+
 		setFeedback(
 			feedback.map((item) => {
 				if (item.id === id) {
-					return { ...item, ...updItem }
+					return { ...item, ...data }
 				} else {
 					return item
 				}
@@ -50,6 +82,7 @@ export const FeedbackProvider = ({ children }) => {
 			value={{
 				feedback,
 				feedbackEdit,
+				isLoading,
 				deleteFeedback,
 				addFeedback,
 				editFeedback,
