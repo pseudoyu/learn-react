2a1ee0c301b2ba4dbfda6160dcad62e830433adb
pseudoyu <pseudoyu@connect.hku.hk>
2022-12-13 04:24:06 +0800

    feat: add basic pages and signin

---
+++ house-marketplace/src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBGM3DC3SAfQhrEe1wyh0HNEet6agES9jw',
+	authDomain: 'house-marketplace-d692c.firebaseapp.com',
+	projectId: 'house-marketplace-d692c',
+	storageBucket: 'house-marketplace-d692c.appspot.com',
+	messagingSenderId: '693228718079',
+	appId: '1:693228718079:web:1e91fcc8aa372610d5c9ca',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
---
+++ house-marketplace/src/pages/SignIn.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
+const SignIn = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = formData
+
+    const navigate = useNavigate()
+
+    return (
+        <>
+            <div className="pageContainer">
+                <header>
+                    <p className="pageHeader">
+                        Welcome Back!
+                    </p>
+                </header>
+            </div>
+        </>
+    )
+}
+
+export default SignIn
