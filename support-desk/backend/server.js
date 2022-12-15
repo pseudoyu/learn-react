6cb6651df1d8fcde144b092d269507afba5df8cd
pseudoyu <pseudoyu@connect.hku.hk>
2022-12-16 03:10:59 +0800

    feat: basic frontend layout & backend user models done

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,1 +1,24 @@
-console.log('Server Is Running...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome to the support desk api' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`)
+})
