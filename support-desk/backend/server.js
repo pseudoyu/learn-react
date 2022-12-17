30b918a192a310dd21a82d25fe0c5f6ea1f26369
pseudoyu <pseudoyu@connect.hku.hk>
2022-12-17 15:27:09 +0800

    feat: add prod config

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
@@ -11,12 +12,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-	res.status(200).json({ message: 'Welcome to the support desk api' })
-})
-
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+	app.get('*', (req, res) => {
+		res.sendFile(__dirname, 'frontend', 'build', 'index.html')
+	})
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to the support desk api' })
+	})
+}
 
 app.use(errorHandler)
 
